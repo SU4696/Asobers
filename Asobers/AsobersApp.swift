8821b132b27e18e7eb2527202af6768d5ecd8984
Suyeon Cho <suyeon4696@gmail.com>
2023-10-17 11:53:25 +0200

    Initial Commit

---
+++ Asobers/AsobersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsobersApp.swift
+//  Asobers
+//
+//  Created by Suyeon Cho on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AsobersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
