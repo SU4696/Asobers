5d82f5cf9aa2c08288bbc703d3113c585ba93ff0
Gabriele Perillo <gabry.perillo@hotmail.com>
2023-10-20 11:59:24 +0200

    Test DeleteView

---
+++ Asobers/View/DeleteView.swift
@@ -0,0 +1,18 @@
+//
+//  DeleteView.swift
+//  Asobers
+//
+//  Created by Gabriele Perillo on 20/10/23.
+//
+
+import SwiftUI
+
+struct DeleteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeleteView()
+}
