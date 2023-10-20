53c58e62e39a43bc08ef294887834f50681ecca4
Suyeon Cho <suyeon4696@gmail.com>
2023-10-20 12:15:56 +0200

    item data model file

--- Asobers/View/itemModel.swift
+++ Asobers/View/itemModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct Learner: Identifiable {
+    var id: UUID = UUID()
+    var date: String
+    var status: String
+    var name: String
+    var measurement: String
+    var amount: String
+    var reminderStatus: String
+    var frequency: Int
+    var progress: Int
+    var goal: Int
+    var favouriteColour: Color = .black
+    var iconName: String = "noDescription"
+}
