a2fe5684a6838321235e91020ae3ea14cf4dd8ae
Gabriele Perillo <gabry.perillo@hotmail.com>
2023-10-20 12:29:02 +0200

    Change itemModel

--- Asobers/View/itemModel.swift
+++ Asobers/View/itemModel.swift
@@ -11,11 +11,11 @@ import SwiftUI
 struct Learner: Identifiable {
     var id: UUID = UUID()
     var date: String
-    var status: String
+    var status: Bool
     var name: String
     var measurement: String
     var amount: String
-    var reminderStatus: String
+    var reminderStatus: Bool
     var frequency: Int
     var progress: Int
     var goal: Int
