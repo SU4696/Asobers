9c15a14a884bd84888389a455d8ab5a3b0b335ac
Suyeon Cho <suyeon4696@gmail.com>
2023-10-20 11:36:50 +0200

    gabreiele's change and cateogorize folders

---
+++ Asobers/View/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  Asobers
+//
+//  Created by Suyeon Cho on 17/10/23.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("New Page")
+            .font(.title)
+            .navigationBarTitle("Calendar")
+    }
+}
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        
+        NavigationView {
+            VStack {
+               
+                
+            }
+            
+            
+            
+            .navigationBarTitle("Today", displayMode: .inline)
+            .navigationBarItems(
+                leading: NavigationLink(destination:  SecondView()){
+                    
+                    Image(systemName:"calendar")
+                        .bold()
+                        .foregroundColor(.pink)
+                    
+                }
+                ,
+                trailing: NavigationLink(destination:  SecondView()){
+                    
+                    Image(systemName:"plus")
+                        .bold()
+                        .foregroundColor(.pink)
+                }
+            )
+            
+        }
+    }
+
+}
+
+#Preview {
+    ContentView()
+}
