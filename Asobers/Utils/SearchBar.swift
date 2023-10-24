2829c4b513ffffe43788dc15aa713df38f8325f4
Suyeon Cho <suyeon4696@gmail.com>
2023-10-24 09:02:24 +0200

    creation of color and Icon selection popover

--- Asobers/Utils/SearchBar.swift
+++ Asobers/Utils/SearchBar.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct SearchBar: View {
+    
+    @Binding var searchText: String
+    @State private var isEditing = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            
+            TextField("Search...", text: $searchText)
+                .padding(7)
+                .padding(.horizontal, 15)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .padding(.horizontal, 10)
+                .onTapGesture {
+                    self.isEditing = true
+                }
+            
+            if isEditing {
+                Button(action: {
+                    self.isEditing = false
+                    self.searchText = ""
+                }) {
+                    Text("Cancel")
+                }
+                .padding(.trailing, 10)
+                .transition(.move(edge: .trailing))
+                
+            }
+        }
     }
 }
 
 #Preview {
-    SearchBar()
+    SearchBar(searchText:  .constant(""))
 }
