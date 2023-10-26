6e7e9349cbac6d53b2f7e9d334a02bd8923fb3d2
Maryova <Maryvsl500@gmail.com>
2023-10-26 11:45:08 +0200

    onboarding second view

---
+++ Asobers/View/OnboardingSecondView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingSecondView.swift
+//  Asobers
+//
+//  Created by Maryam Vasilehbar on 10/26/23.
+//
+
+import SwiftUI
+
+struct OnboardingSecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingSecondView()
+}
