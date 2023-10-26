62e467148a545f13c975100b9f3377a72ce4a13d
Maryova <Maryvsl500@gmail.com>
2023-10-26 11:45:42 +0200

    Onboarding second page

--- Asobers/View/OnboardingSecondView.swift
+++ Asobers/View/OnboardingSecondView.swift
@@ -8,8 +8,56 @@
 import SwiftUI
 
 struct OnboardingSecondView: View {
+    
+    @State private var singleSelection: UUID?
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Let's Start Together!")
+            .bold()
+            .font(.largeTitle)
+            .foregroundStyle(.pink)
+            .padding(.top,60)
+        
+        NavigationView {
+            List(selection: $singleSelection) {
+                Section(header:
+                            Text("Goal")
+                    .font(.system(size: 19, weight: .bold))
+                    .foregroundStyle(.black)
+                )
+                {
+                    Text("Water")
+                }
+                
+                Section(header:
+                            Text("Limits")
+                    .font(.system(size: 18, weight: .bold))
+                    .foregroundStyle(.black)
+                ) {
+                    Text("Coffee")
+                    Text("Tabacco")
+                }
+            }
+            
+        }
+        
+        Button(action: {
+            //going to next page
+        } ) {
+            Text ("Set")
+                .bold()
+                .padding(.horizontal, 150)
+                .padding(.vertical, 20)
+                .foregroundColor(.white)
+                .background(Color.pink)
+            //change the main color
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .shadow(radius: 10)
+            
+            
+            
+        }
     }
 }
 
